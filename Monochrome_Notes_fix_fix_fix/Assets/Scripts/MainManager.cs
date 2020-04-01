a172fc3033428221d9625fdba974c1183c007a73
miisa <32995234+kamiimiisa@users.noreply.github.com>
2020-04-01 13:59:49 +0900

    mainの更新
    main の更新

--- Monochrome_Notes_fix_fix_fix/Assets/Scripts/MainManager.cs
+++ Monochrome_Notes_fix_fix_fix/Assets/Scripts/MainManager.cs
@@ -9,11 +9,14 @@ using Monochrome_Notes;
 
 public class MainManager : MonoBehaviour
 {
+    private float carrentGameTime = 0;
 
     private float deltaTime;
     private float noteSpeed = 1;
 
     private string musicName = "tutorial"; //テストで入れてるけどほんとは曲選択画面からもらう
+
+    private bool musicStart = false;
 
     [SerializeField] private GameObject noteObj;
     [SerializeField] private GameObject judgeLineObj;
@@ -30,6 +33,13 @@ public class MainManager : MonoBehaviour
         {LineNum.Line4, 3.75f}
     };
 
+    [SerializeField] private List<AudioClip> bgmList;
+    [SerializeField] private List<AudioClip> seList;
+    private AudioSource audioSource;
+    private int musicNum = 0;
+
+    float carrentTimeTest = 0f;
+
     // Use this for initialization
     void Start()
     {
@@ -38,15 +48,27 @@ public class MainManager : MonoBehaviour
         //deltaTime = gameMaster.GetTimeDeltaTime();
         //noteSpeed = gameMaster.GetNoteSpeed();
 
+        deltaTime = Time.deltaTime;
+
+        audioSource = GetComponent<AudioSource>();
+        for (int i = 0; i < bgmList.Count; i++)
+        {
+            if (bgmList[i].name == musicName)
+            {
+                audioSource.clip = bgmList[i];
+            }
+        }
+
         string json = Resources.Load<TextAsset>(musicName).ToString();
         editData = JsonUtility.FromJson<MusicDTO.EditData>(json);
         note = JsonUtility.FromJson<MusicDTO.Note>(json);
 
         for (int i = 0; i < note.notes.Count; i++)
         {
+            //carrentTime = ノーツの最短間隔[60f / (BPM * LPB)] * ノーツの位置
             float carrentTime = 60f / (editData.BPM * note.notes[0].LPB) * note.notes[i].num;
             NOTES_LIST.Add(new MyNote.NotePos(carrentTime, (LineNum)Enum.ToObject(typeof(LineNum), note.notes[i].block)));
-            Debug.Log(NOTES_LIST[i].notesTimeg.ToString() + NOTES_LIST[i].lineNum);
+            //Debug.Log(NOTES_LIST[i].notesTimeg.ToString() + NOTES_LIST[i].lineNum);
         }
 
         Initialize();
@@ -55,6 +77,16 @@ public class MainManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (carrentGameTime < 5)
+        {
+            carrentGameTime += deltaTime;
+        }
+        else if (!musicStart)
+        {
+            audioSource.Play();
+            musicStart = true;
+        }
 
     }
 
