40b956803cc5428031876fa5c9385ff9e4d76bea
Kusanagi241 <62680393+Kusanagi241@users.noreply.github.com>
2020-04-07 15:48:13 +0900

    TitleManagerの復帰
    スクリプトの復帰

--- Monochrome_Notes_fix_fix_fix/Assets/Scripts/SceneManager/TitleManager.cs
+++ Monochrome_Notes_fix_fix_fix/Assets/Scripts/SceneManager/TitleManager.cs
@@ -2,15 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TitleManager : MonoBehaviour {
+public class TitleManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private enum Select
+    {
+        Start, Tutorial, Exit
+    }
+    private Select select = Select.Start;
+    [SerializeField] GameObject[] icon = new GameObject[3];
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (select)
+        {
+            case Select.Start:
+                DownSelect(Select.Tutorial);
+                //ここにシーン遷移の処理
+                break;
+            case Select.Tutorial:
+                UpSelect(Select.Start);
+                DownSelect(Select.Exit);
+                //ここにシーン遷移の処理
+                break;
+            case Select.Exit:
+                UpSelect(Select.Tutorial);
+                //ここにシーン遷移の処理
+                break;
+            default:
+                Debug.Log("バグです");
+                break;
+        }
+    }
+
+    void UpSelect(Select _select)
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            select = _select;
+            IconSelect(_select);
+        }
+    }
+
+    void DownSelect(Select _select)
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            select = _select;
+            IconSelect(_select);
+        }
+    }
+
+    void IconSelect(Select _select)
+    {
+        for (int i = 0; i < icon.Length; i++)
+        {
+            icon[i].SetActive(false);
+        }
+        icon[(int)System.Enum.ToObject(typeof(int), _select)].SetActive(true);
+    }
 }
+
