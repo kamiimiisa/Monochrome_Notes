a6674ea4bc9976fbe7bd84b980a1cdfb34b30eba
Kusanagi241 <62680393+Kusanagi241@users.noreply.github.com>
2020-05-04 17:10:55 +0900

    Update GameMaster.cs

--- Monochrome_Notes_fix_fix_fix/Assets/Scripts/GameMaster.cs
+++ Monochrome_Notes_fix_fix_fix/Assets/Scripts/GameMaster.cs
@@ -2,27 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMaster : MonoBehaviour {
+public class GameMaster : MonoBehaviour
+{
 
-    private float noteSpeed;
-    private string musicName;
-
-    public void SetNoteSpeed(float _speed) {
-        noteSpeed = _speed;
-    }
-    public float GetNoteSpeed() {
-        return noteSpeed;
+    private static string musicName;
+    public static string MusicName
+    {
+        get { return musicName; }
+        set { musicName = value; }
     }
 
+    private static int score;
+    public static int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
 
+    private static int maxCombo;
+    public static int MaxCombo
+    {
+        get { return maxCombo; }
+        set { maxCombo = value; } 
+    }
+
+    private static int parfectNum;
+    public static int ParfectNum
+    {
+        get { return parfectNum; }
+        set { parfectNum = value; }
+    }
+
+    private static int greatNum;
+    public static int GreatNum
+    {
+        get { return greatNum; }
+        set { greatNum = value; }
+    }
+
+    private static int missNum;
+    public static int MissNum
+    {
+        get { return missNum; }
+        set { missNum = value; }
+    }
+
+    private static float noteSpeed;
+    public static float NoteSpeed
+    {
+        get { return noteSpeed; }
+        set { noteSpeed = value; }
+    }
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
     }
 }
